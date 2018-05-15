265034c1ffe855d8d58b5622e37d9c6d7e855f24
bill <billwu_@outlook.com>
2018-05-15 19:29:40 +0000

    orangized all docs

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import ChatBar from './ChatBar.jsx';
 class App extends Component {
   render() {
     return (
+      <div>
       <nav className="navbar">
         <a href="/" className="navbar-brand">Chatty</a>
       </nav>
       <Message />
       <ChatBar />
+      </div>
     );
   }
 }
