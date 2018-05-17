d97406045e82f89411a6fb7e32be9c97be560305
bill <billwu_@outlook.com>
2018-05-17 01:56:13 +0000

    finished project without stretch

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -16,34 +16,56 @@ const server = express()
 // Create the WebSockets server
 const wss = new WebSocket.Server({ server });
 
+let usercount = 0;
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
-
+  usercount += 1;
   //read message
    ws.on('message', function incoming(message) {
     const myMessage = JSON.parse(message);
+    console.log(myMessage);
     let messageObject = {};
-    if(myMessage.messageType === "message"){
+    switch(myMessage.messageType){
+      case "message" :
       messageObject = {
         id: getId(),
         username: myMessage.username,
         content: myMessage.message,
+        messageType: myMessage.messageType,
       };
-    } else{
+      break;
+      case "notification":
       messageObject = {
         id: getId(),
         oldname: myMessage.oldname,
-        name: myMessage.username,
+        username: myMessage.username,
+        messageType: myMessage.messageType,
       };
+      break;
+      default:
+        // show an error in the console if the message type is unknown
+        throw new Error("Unknown event type " + data.type);
     }
     wss.broadcast(messageObject);
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    usercount -= 1;
+    wss.broadcast({
+      usercount:usercount,
+      messageType: "userCount",
+    });
+    console.log('Client disconnected');
+  });
+  wss.broadcast({
+    usercount:usercount,
+    messageType: "userCount",
+  });
 });
 
 //generate and Id for each message send to us
