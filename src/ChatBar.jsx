56bd9c73e7029d40a2455a3f5486ffdb9a7063f2
billywoo17 <billwu_@outlook.com>
2018-05-17 17:46:59 -0700

    added screenshots

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -20,24 +20,16 @@ class ChatBar extends Component {
   }
 
   myUsername(event){
-    if(event.key == 'Enter'){
       this.props.myUsername(event.target.value);
-    }
   }
+
   render() {
-
-
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" onKeyPress={this.myUsername} placeholder="Anonymous" />
+        <input className="chatbar-username" onBlur={this.myUsername} placeholder="Anonymous" />
         <input  onKeyPress={this.onChange} className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
   }
 }
 export default ChatBar;
-
-
-
-
-
